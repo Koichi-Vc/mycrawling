8e19cff2e985a71374879daebf0a7d9c7838dbdb
Fukuro Loce <wslerne@gmail.com>
2025-04-20 23:12:15 +0900

    include_package_dataを追加。

--- setup.py
+++ setup.py
@@ -4,7 +4,8 @@ from setuptools import setup, find_namespace_packages
 setup(
     name='mycrawling',
     version='0.0.0',
-    packages=find_namespace_packages()
+    packages=find_namespace_packages(),
+    include_package_data=True
 )
 
 print(f'find_namespace_packages: {find_namespace_packages()}')
