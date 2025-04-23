f40e4eb96b66442368757dfb0c2b22bc6e66df42
Koichi-Vc <wslerne@gmail.com>
2025-04-24 00:20:48 +0900

    デバッグ用のログ設定ミスを修正。

--- mycrawling/logs/debug_log.py
+++ mycrawling/logs/debug_log.py
@@ -1,8 +1,8 @@
 from logging import getLogger,INFO, DEBUG
-from logging import StreamHandler
+from logging import StreamHandler, Formatter
 
 
-formatter = 'Debug logs. %(filename)s | %(funcName)s | %(lineno)d | %(message)s'
+formatter = Formatter('Debug logs:  %(filename)s | %(funcName)s | %(lineno)d | %(message)s')
 
 debug_logger = getLogger(__name__)
 handler = StreamHandler()
@@ -12,9 +12,14 @@ debug_logger.setLevel(INFO)
 
 #debug_loggerとconfパッケージ間の循環インポート問題を次回解決する。
 def get_debug_log(debug=False):
-    global debug_logger 
+    global debug_logger
+    for handler in debug_logger.handlers[:]:
+        if len(debug_logger.handlers) >= 2:
+            debug_logger.removeHandler(handler)
+
     if debug is True:
-        debug_logger.setLevel(DEBUG)    
+        debug_logger.handlers[0].setLevel(DEBUG)
+        debug_logger.setLevel(DEBUG)
     return debug_logger
 
 
