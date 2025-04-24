c1b115eb6d008b12fea90f593a588c91647caecc
Koichi-Vc <wslerne@gmail.com>
2025-04-25 01:00:40 +0900

    ハンドラが重複して登録されるミスを修正。

--- mycrawling/logs/debug_log.py
+++ mycrawling/logs/debug_log.py
@@ -1,25 +1,32 @@
-from logging import getLogger,INFO, DEBUG
+from logging import getLogger, DEBUG
 from logging import StreamHandler, Formatter
 
 
-formatter = Formatter('Debug logs:  %(filename)s | %(funcName)s | %(lineno)d | %(message)s')
-
 debug_logger = getLogger(__name__)
-handler = StreamHandler()
-handler.setFormatter(formatter)
-debug_logger.addHandler(handler)
-debug_logger.setLevel(INFO)
 
-#debug_loggerとconfパッケージ間の循環インポート問題を次回解決する。
-def get_debug_log(debug=False):
+def setting_debug_log(debug=False, formatter=None, logger=None, handler=None, **kwargs):
     global debug_logger
-    for handler in debug_logger.handlers[:]:
-        if len(debug_logger.handlers) >= 2:
-            debug_logger.removeHandler(handler)
 
+    if not formatter:
+        formatter = Formatter('Debug logs:  %(filename)s | %(funcName)s | %(lineno)d | %(message)s')
+    if logger:
+        debug_logger = logger
+    
+    if not handler:
+        handler = StreamHandler()
+        handler.setFormatter(formatter)
     if debug is True:
-        debug_logger.handlers[0].setLevel(DEBUG)
+        handler.setLevel(DEBUG)
         debug_logger.setLevel(DEBUG)
+
+    if debug_logger.handlers:
+        for handler in debug_logger.handlers[:]:
+            debug_logger.removeHandler(handler)
+    debug_logger.addHandler(handler)
+    return debug_logger
+
+def get_debug_logger():
+
     return debug_logger
 
 
