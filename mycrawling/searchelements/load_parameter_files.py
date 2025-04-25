6e394310999a35ac66eb8b3f654a58d23975ecea
Koichi-Vc <wslerne@gmail.com>
2025-04-26 02:01:55 +0900

    get_filter_parameters関数を廃止。グローバル変数のdefault_load_file, user_parameters_file_path, elements_filter_parametersを削除。

--- mycrawling/searchelements/load_parameter_files.py
+++ mycrawling/searchelements/load_parameter_files.py
@@ -1,29 +1,10 @@
 from pathlib import Path
 from mycrawling.utils.loaders.loader import json_load, ref_files_load, FilesLoader
 from mycrawling.logs.debug_log import debug_logger
+from mycrawling.conf.data_setting import ref_dataconfig
 ''' 要素検索のフィルターを作成する為の引数をまとめたjsonファイルを読み込む。 '''
 
-
-default_load_file = 'mycrawlingpack/parameter_files/elements_filter_arguments.json'
-
-user_parameters_file_path = Path('user_reftexts_dir/elements_filter_arguments.json')#ユーザーが用意する要素検索フィルター用のパラメータファイル
-
-elements_filter_parameters = dict()
-
-#get_filters_parametersの新コード
-def get_filter_parameters():
-    ''' フィルターに用いるパラメータ値をまとめたjsonファイルを読み込む '''
-    global elements_filter_parameters
-    #parameterhandler_cls = None#parameterhandlerクラス
-    #is_required_param = None#parameterhandlerが引数を必須とするかチェック
-    load_file = user_parameters_file_path if user_parameters_file_path.is_dir() else default_load_file
-    elements_filter_parameters = ref_files_load(load_file, json_load, encoding='UTF-8')
-
-    debug_logger.debug(f'elements_filter_parameters: {elements_filter_parameters}')
-    if 'None' in elements_filter_parameters.keys():
-        #キー名'None'をNoneに変換。特にタグを指定していないフィルターを表す。
-        elements_filter_parameters[None] = elements_filter_parameters['None']
-    return elements_filter_parameters
+'''パスがすべて実質相対パスになってしまっている問題を解決する。 25/04/23'''
 
 
 class FilterParameterLoader(FilesLoader):
@@ -35,18 +16,19 @@ class FilterParameterLoader(FilesLoader):
     '''
 
     default_load_method = None#デフォルトで使用するファイル読み込み用メソッド。
-    default_load_file = Path('mycrawling/parameter_files/elements_filter_arguments.json')
+    default_load_file = Path(ref_dataconfig.get_conf_value('FILTER_PARAMETER_FILE', default=''))
     
     def __init__(self, user_parameter_file_path=None, option='r', encoding='UTF-8', load_method=None, **kwargs):
-        self.user_parameter_file_path = Path(user_parameter_file_path)
+        self.user_parameter_file_path = Path.cwd().joinpath(Path(user_parameter_file_path))
         self.option = option
         self.encoding = encoding
         self.load_method = load_method if callable(load_method) else self.default_load_method
         self.elements_filter_parameters = None
     
     def load_filter(self, **kwargs):
-
-        load_file = self.user_parameter_file_path if self.user_parameter_file_path.is_dir() else self.default_file_path
+        print(f'FilterParameterLoader.load_filter>>>>>')
+        print(f'self: {self} | kwargs: {kwargs}')
+        load_file = self.user_parameter_file_path if self.user_parameter_file_path.is_dir() else self.default_load_file
         elements_filter_parameters = self.file_load(
             load_file,
             option=self.option,
@@ -60,10 +42,5 @@ class FilterParameterLoader(FilesLoader):
 
         return self.elements_filter_parameters
 
-
-if __name__ == '__main__':
-    get_filter_parameters()
-    debug_logger.debug(f'if __name__ == __main__>>>')
-    debug_logger.debug(F'elements_filter_parameters: {elements_filter_parameters}')
 
     
