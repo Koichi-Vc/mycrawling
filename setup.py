0936b37e62bb68142c3088edb12e85e4d6b02d81
Fukuro Loce <wslerne@gmail.com>
2025-05-13 00:36:26 +0900

    インストール時にREADME.mdをMETADATA, サードパーティライセンス情報をlicensesに含める様に変更。

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
 from setuptools import setup, find_namespace_packages
+from pathlib import Path
 
+this_directory = Path(__file__).parent
+description = (this_directory / "README.md").read_text(encoding='UTF-8')
+#description = Path("README.md").read_text(encoding="UTF-8")
 
 setup(
     name='mycrawling',
     version='0.0.0',
     packages=find_namespace_packages(),
-    include_package_data=True
+    include_package_data=True,
+    long_description = description,
+    long_description_content_type = 'text/markdown',
+    license_files = ['NOTICE', 'third_party_license/Third_Party_License.txt']
 )
-
-
-
