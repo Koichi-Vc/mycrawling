eaa5773ad321a354bd97fa7b4ebccec0339eff0f
Fukuro Loce <wslerne@gmail.com>
2025-10-06 02:13:08 +0900

    style: developブランチからのマージで発生したコンフリクトの解消。

--- mycrawling/evaluations/evaluationtexts.py
+++ mycrawling/evaluations/evaluationtexts.py
@@ -5,10 +5,6 @@ from mycrawling.scorings.texts import ScoringTexts
 from mycrawling.logs.debug_log import debug_logger
 
 
-<<<<<<< HEAD
-=======
-
->>>>>>> develop
 class EvaluateTexts(ScoreEvaluations, SelectListOperator):
     ''' 文字列の評価 '''
     
